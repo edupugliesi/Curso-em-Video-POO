940b96f15903ddcae8fce6a22e16e7a77dadd61a
Eduardo Pugliesi <70285858+edupugliesi@users.noreply.github.com>
2023-08-22 15:19:49 -0300

    Aula04

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,19 @@
+package aula04;
+
+
+public class Aula04 {
+
+  
+    public static void main(String[] args) {
+            
+        Caneta c1 = new Caneta("NIC", 0.4f, "Azul"); 
+        System.out.println("Modelo: " + c1.getModelo());
+        System.out.println("Ponta: " + c1.getPonta());
+        System.out.println("Cor: "+ c1.getCor());
+        
+   
+        
+        
+    }
+    
+}
