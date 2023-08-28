083db00d241bef00feec29bd75ea24f3da2e9737
Eduardo Pugliesi <70285858+edupugliesi@users.noreply.github.com>
2023-08-28 14:38:11 -0300

    Aula06

---
+++ Aula06/src/aula06/ControleRemoto.java
@@ -0,0 +1,126 @@
+package aula06;
+public class ControleRemoto implements Controlador {
+    //Atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+    
+    
+    //Construtores
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+    
+    
+    //Getters e Setters
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean isLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean isTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+    
+    
+    //Métodos Abstratos
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("----MENU----");
+        System.out.println("Está ligado? " + this.isLigado());
+        System.out.println("Está tocando? " + this.isTocando());
+        System.out.print("Volume: " + this.getVolume());
+        
+        for (int i = 1; i <= this.getVolume(); i+=10){
+            System.out.print("| ");
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu.");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.isLigado() == true){
+            this.setVolume(this.getVolume()+10);
+        } else {
+            System.out.println("Impossível aumentar o volume.");
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.isLigado() == true){
+            this.setVolume(this.getVolume()-10);
+        } else {
+            System.out.println("Impossível reduzir o volume.");
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.isLigado() && this.getVolume() > 0){
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.isLigado() && this.getVolume() == 0){
+            this.setVolume(5);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.isLigado() && !this.isTocando()){
+            this.setTocando(true);
+        } else {
+            System.out.println("Não consegui reproduzir.");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.isLigado() && this.isTocando()){
+            this.setTocando(false);
+        } else {
+            System.out.println("Não consegui pausar.");
+        }
+    }
+    
+    
+}
+
+
